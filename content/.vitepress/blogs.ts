cc05652b0dab0226ae78f28b2725047ff58f94de
aadi58002 <aadi58002gmail.com>
2022-10-10 21:41:18 +0530

    Fixing all the paths in the config

--- content/.vitepress/blogs.ts
+++ content/.vitepress/blogs.ts
@@ -6,7 +6,7 @@ export default () => {
     Uniqtags: ['Vim','Text Editor'],
     items: [
       {
-        links: '/blogs/Vim',
+        links: '/Linux-Blog/blogs/Vim',
         text: 'Vim Tricks',
         description: 'A set of vim command which can be useful but are not commonly found',
         tags: ['Text Editor', 'Vim'],
