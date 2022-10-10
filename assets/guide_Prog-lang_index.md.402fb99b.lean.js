28ac41bfc722243b14a91852db3d8b52edafbc60
aadi58002 <aadi58002@users.noreply.github.com>
2022-10-10 15:07:24 +0000

    deploy: a23dea95ad3f59020fa29aa1c788e3c38c6fe9b8

---
+++ assets/guide_Prog-lang_index.md.402fb99b.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as a}from"./app.6bb314e1.js";const l=JSON.parse('{"title":"Index","description":"","frontmatter":{"title":"Index"},"headers":[],"relativePath":"guide/Prog-lang/index.md","lastUpdated":1665414386000}'),n={name:"guide/Prog-lang/index.md"};function r(o,d,s,c,i,p){return t(),a("div")}const m=e(n,[["render",r]]);export{l as __pageData,m as default};
