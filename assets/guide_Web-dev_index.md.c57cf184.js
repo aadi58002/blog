240b71c9df75c23f1d86c40fea4f3aeaf9018a6e
aadi58002 <aadi58002@users.noreply.github.com>
2022-10-10 14:13:48 +0000

    deploy: 5c88be4bcad6fd5fd2c858847a96228743045070

--- assets/guide_Web-dev_index.md.c57cf184.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as a}from"./app.0d259c8f.js";const l=JSON.parse('{"title":"Index","description":"","frontmatter":{"title":"Index"},"headers":[],"relativePath":"guide/Web-dev/index.md","lastUpdated":1665411015000}'),d={name:"guide/Web-dev/index.md"};function n(r,o,s,c,i,p){return t(),a("div")}const m=e(d,[["render",n]]);export{l as __pageData,m as default};
