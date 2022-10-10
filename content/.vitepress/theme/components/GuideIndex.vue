e440a6efda68135006f90ee3b3094d8af21dbe2c
aadi58002 <aadi58002gmail.com>
2022-10-10 21:50:09 +0530

    Fixing a spelling mistake in a link

--- content/.vitepress/theme/components/GuideIndex.vue
+++ content/.vitepress/theme/components/GuideIndex.vue
@@ -5,10 +5,10 @@ import { sidebarGuideLinux, sidebarGuideWebdev, sidebarGuideEmacs ,sidebarGuideP
 
 <template>
     <div class="container">
-        <GuideTopicsList :list="sidebarGuideLinux()[0]" color="red" image="/Linux-blog/arch-logo.webp" imageWidth="40%"/>
-        <GuideTopicsList :list="sidebarGuideWebdev()[0]" color="green"  image="/Linux-blog/web-dev.webp" imageWidth="30%"/>
-        <GuideTopicsList :list="sidebarGuideEmacs()[0]" color="blue" image="/Linux-blog/doom-emacs.webp" imageWidth="30%"/>
-        <GuideTopicsList :list="sidebarGuideProglang()[0]" color="yellow" image="/Linux-blog/programming-lang.webp" imageWidth="90%"/>
+        <GuideTopicsList :list="sidebarGuideLinux()[0]" color="red" image="/Linux-Blog/arch-logo.webp" imageWidth="40%"/>
+        <GuideTopicsList :list="sidebarGuideWebdev()[0]" color="green"  image="/Linux-Blog/web-dev.webp" imageWidth="30%"/>
+        <GuideTopicsList :list="sidebarGuideEmacs()[0]" color="blue" image="/Linux-Blog/doom-emacs.webp" imageWidth="30%"/>
+        <GuideTopicsList :list="sidebarGuideProglang()[0]" color="yellow" image="/Linux-Blog/programming-lang.webp" imageWidth="90%"/>
     </div>
 </template>
 
