55afdc6f3b95a46040082b3a85b4b3e483da0896
aadi58002 <aadi58002gmail.com>
2023-06-05 10:28:08 +0530

    fixup! Some fixes for broken links

--- content/.vitepress/blogs.ts
+++ content/.vitepress/blogs.ts
@@ -3,24 +3,24 @@ export default () => {
     text: 'Blogs',
     collapsible: true,
 // Python Script Adjustment Block Start
-    Uniqtags: ['seaorm','kubernetes','rust','Vim','Text Editor','podman','database','postgres','docker','devops'],
+    Uniqtags: ['seaorm','Vim','devops','podman','docker','postgres','database','Text Editor','rust','kubernetes'],
     items: [
       {
-        link: '/Linux-Blog/blogs/Vim',
+        links: '/blogs/Vim',
         text: 'Vim Tricks',
         description: 'A set of vim command which can be useful but are not commonly found',
         tags: ['Text Editor', 'Vim'],
         lastUpdated: '2023-06-05'
       },
       {
-        link: '/Linux-Blog/blogs/DevOps-tools',
+        links: '/blogs/DevOps-tools',
         text: 'Dev Ops Tools',
         description: 'A list of dev ops tools and what they and what are there advantages or disadvantages',
         tags: ['docker','podman','devops','kubernetes'],
         lastUpdated: '2023-06-02'
       },
       {
-        link: '/Linux-Blog/blogs/Seaorm',
+        links: '/blogs/Seaorm',
         text: 'Seaorm',
         description: 'This blog will describe the process of me learning about Seaorm',
         tags: ['podman','database','postgres','rust','seaorm'],
