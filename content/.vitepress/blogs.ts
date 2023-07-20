91a858d25c750d8c2b10984077762b648d601188
aadi58002 <aadi58002@gmail.com>
2023-07-20 13:34:13 +0530

    Updating tags and start writing Advent of code 2022 blog

--- content/.vitepress/blogs.ts
+++ content/.vitepress/blogs.ts
@@ -3,14 +3,21 @@ export default () => {
     text: 'Blogs',
     collapsible: true,
 // Python Script Adjustment Block Start
-    Uniqtags: [ 'Text Editor','Vim'],
+    Uniqtags: [ 'programming','nom','Vim','rust','Text Editor'],
     items: [
+      {
+        link: '/blogs/Advent-of-code-2022',
+        text: 'Advent of Code 2022',
+        description: 'Solving Advent of code problems with rust',
+        tags: ['rust', 'programming', 'nom'],
+        lastUpdated: '2023-07-20'
+      },
       {
         link: '/blogs/Vim',
         text: 'Vim Tricks',
         description: 'A set of vim command which can be useful but are not commonly found',
         tags: ['Text Editor', 'Vim'],
-        lastUpdated: '2023-06-05'
+        lastUpdated: '2023-06-28'
       },
     ]
 // Python Script Adjustment Block End
---
+++ content/blogs/Advent-of-code-2022.md
@@ -0,0 +1,17 @@
+---
+title: Advent of Code 2022
+description: Solving Advent of code problems with rust
+production: false
+tags: ['rust', 'programming', 'nom']
+---
+
+# {{ $frontmatter.title }}
+## Why
+
+- I want to get better at writing rust.
+- Learn about parser combinator libraries like neo
+- Get some practice with Data Structure and Algorithms.
+
+## Cheating
+I know in the spirit of question we are supposed to do them on our own.  
+But if i get stuck i will be using the [https://fasterthanli.me/series/advent-of-code-2022](fasterthanlime series: Advent of code 2022) as a fallback to look at good practices or solutions.
