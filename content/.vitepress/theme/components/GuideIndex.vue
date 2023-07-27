a4f2e356ff4b92623ee590c312154ad291b1044a
aadi58002 <aadi58002@gmail.com>
2023-07-27 15:17:37 +0530

    Removing /public from path

--- content/.vitepress/theme/components/GuideIndex.vue
+++ content/.vitepress/theme/components/GuideIndex.vue
@@ -5,10 +5,10 @@ import { sidebarGuideLinux, sidebarGuideWebdev, sidebarGuideEmacs ,sidebarGuideP
 
 <template>
     <div class="container">
-        <GuideTopicsList :list="sidebarGuideLinux()[0]" color="red" image="/public/arch-logo.webp" imageWidth="40%"/>
-        <GuideTopicsList :list="sidebarGuideWebdev()[0]" color="green"  image="/public/web-dev.webp" imageWidth="30%"/>
-        <GuideTopicsList :list="sidebarGuideEmacs()[0]" color="blue" image="/public/doom-emacs.webp" imageWidth="30%"/>
-        <GuideTopicsList :list="sidebarGuideProglang()[0]" color="yellow" image="/public/programming-lang.webp" imageWidth="90%"/>
+        <GuideTopicsList :list="sidebarGuideLinux()[0]" color="red" image="/arch-logo.webp" imageWidth="40%"/>
+        <GuideTopicsList :list="sidebarGuideWebdev()[0]" color="green"  image="/web-dev.webp" imageWidth="30%"/>
+        <GuideTopicsList :list="sidebarGuideEmacs()[0]" color="blue" image="/doom-emacs.webp" imageWidth="30%"/>
+        <GuideTopicsList :list="sidebarGuideProglang()[0]" color="yellow" image="/programming-lang.webp" imageWidth="90%"/>
     </div>
 </template>
 
